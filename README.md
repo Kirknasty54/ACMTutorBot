2b8fd7823b697fbdc9332459198f54ab4050ac50
kirknasty54 <kirkpatrick545454@gmail.com>
2025-11-19 14:41:32 -0500

    set up basic mongodb connection to cluster and specific cluster. updated requirements.txt and README.md for current objectives with bot

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 # ACM Tutor Bot 
 * This is a tutor bot for my local ACM chapter's discord server, of which I am the president of. 
 * Students can ask questions and get answers from the tutors. 
+* The bot uses OpenAI's GPT-5 API to generate responses.
+* The bot uses a MongoDB database to store conversation history and to better respond to follow up questions.
+* The bot is hosted on a personal Rasperry Pi 5. 
--- main.py
+++ main.py
@@ -1,15 +1,39 @@
+import asyncio
 from os import getenv
-from openai import OpenAI
+from openai import OpenAI, api_key
 from dotenv import load_dotenv
 import discord
 from discord.ext import commands
-
+from pymongo import MongoClient
+from memori import Memori
+from datetime import datetime
 #load up environment variables
 load_dotenv()
+MONGODB_USER = getenv("MONGODB_USER")
+MONGODB_PASSWORD = getenv("MONGODB_PASSWORD")
 OPENAI_API_KEY = getenv("OPENAI_API_KEY")
 DISCORD_TOKEN = getenv("DISCORD_TOKEN")
 DISCORD_APP_ID = getenv("DISCORD_APP_ID")
 client = OpenAI(api_key=OPENAI_API_KEY)
+memori = Memori()
+memori.openai = client
+from pymongo.mongo_client import MongoClient
+from pymongo.server_api import ServerApi
+
+#mongodb setup
+MONGODB_URI = getenv("MONGODB_URI")
+DB_Name = getenv("DB_NAME")
+mongo_client = MongoClient(MONGODB_URI, server_api=ServerApi('1'))
+
+try:
+    mongo_client.admin.command('ping')
+    print("Pinged your deployment. You successfully connected to MongoDB!")
+    db = mongo_client[DB_Name]
+    db.client.admin.command('ping')
+    db.acm_discord.find_one();
+    print("Pinged the db")
+except Exception as e:
+    print(e)
 
 #set the intents and settings for the bot
 intents = discord.Intents.default()
@@ -20,8 +44,12 @@ messages = []
 SYSTEM_PROMPT = (
     "You are a friendly computer science tutor for the UCM ACM discord server."
     "You should be able to answer simple computer science and data structures questions that users might have."
-    "Should be able to answer students and tell them what errors they're getting mean, what they're doing wrong in a code segement, how to fix it, etc. Stuff along that nature"
+    "Should be able to answer students and tell them what errors they're getting mean, what they're doing wrong in a code segement, how to fix it, etc. Stuff along that nature."
+    "You have persistent memory capabilities, can remember and store information about past conversations."
+    "ALWAYS start by searching your memory for relevant context if it might be needed"
 )
+
+#memory functionality
 
 #what will display when the bot is online
 @bot.event
@@ -35,21 +63,25 @@ async def on_message(message):
     #ignore self messages
     if message.author == bot.user:
         return
-    messages.append(
-        {"role": "user", "content": message.content}
-    )
-
-    print(messages)
     #respond when @ or via !tutor
     if bot.user and (bot.user.mentioned_in(message) or message.content.startswith("!tutor")):
         user_input = message.content.replace(bot.user.mention, "").strip()
+        print(user_input)
         try:
             #ask open ai for a reply
             response = client.responses.create(
                 model="gpt-5-mini",
                 instructions=SYSTEM_PROMPT,
-                input= message.content
+                input= message.content,
             )
+
+            #gives it realistic typing speed, improving user experience knowing the bot is at least responding
+            wpm = 150
+            seconds_per_word = 60 / wpm
+            max_typing_time = 6
+            typing_time = min(len(response.output_text) * seconds_per_word, max_typing_time)
+            async with message.channel.typing():
+                await asyncio.sleep(typing_time)
 
             await message.reply(response.output_text)
             print(response.output_text)
